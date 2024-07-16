b22489ba0f79bc2fcf990cef615a3ada05164680
jjsales <jjsales@uolinc.com>
2024-07-16 09:17:21 -0300

    Exercicio3

---
+++ src/main/java/br/com/dio/poo/exercicio3/Exercicio3.java
@@ -0,0 +1,57 @@
+package br.com.dio.poo.exercicio3;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    // Criar uma nova conta bancária com o saldo inicial
+    double saldoInicial = scanner.nextDouble();
+    ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+    // Lê a Entrada com o valor do deposito e aciona o método "depositar"
+    double valorDeposito = scanner.nextDouble();
+    conta.depositar(valorDeposito);
+
+    // Lê a Entrada com o valor do saque e aciona o método "sacar"
+    double valorSaque = scanner.nextDouble();
+    conta.sacar(valorSaque);
+
+    // Fechar o scanner para evitar vazamentos de recursos
+    scanner.close();
+  }
+}
+
+class ContaBancaria {
+  private double saldo;
+
+  public ContaBancaria(double saldoInicial) {
+    this.saldo = saldoInicial;
+  }
+
+  public void depositar(double valor) {
+    saldo += valor;
+    System.out.println("Deposito feito.");
+    // Incrementar o saldo com o valor e imprimir "Depósito feito."
+    imprimirSaldo();
+  }
+
+  public void sacar(double valor) {
+    if (saldo >= valor) {
+      saldo -= valor;
+      System.out.println("Saque feito.");
+    } else {
+      System.out.println("Saldo insuficiente. Saque não realizado.");
+    }
+    // Verificar se o saldo é suficiente considerando o valor:
+    // Se sim, atualizar o saldo e imprimir "Saque feito."
+    // Se não, imprimir "Saldo insuficiente. Saque não realizado."
+    imprimirSaldo();
+  }
+
+  private void imprimirSaldo() {
+    System.out.printf("Saldo atual: %.1f\n", this.saldo);
+  }
+}
